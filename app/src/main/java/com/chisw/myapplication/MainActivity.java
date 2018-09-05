a04a9d52b8aae4b3fd790be737046fdd19c31ddc
Vitaly Zeenko <vitaly.zeenko@chisw.com>
2018-09-05 13:09:24 +0300

    Add output of user's photo using Glide.

--- app/src/main/java/com/chisw/myapplication/MainActivity.java
+++ app/src/main/java/com/chisw/myapplication/MainActivity.java
@@ -1,14 +1,17 @@
 package com.chisw.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private SignInButton signInButton;
     private GoogleApiClient apiClient;
     private Button logout;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         setContentView(R.layout.login_activity);
         signInButton = findViewById(R.id.sign_in_button);
         logout = findViewById(R.id.logout);
+        imageView = findViewById(R.id.imageView);
+
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,11 +78,21 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private void handleResult(GoogleSignInResult signInResult) {
         if (signInResult.isSuccess()) {
             GoogleSignInAccount account = signInResult.getSignInAccount();
-            String name = account.getDisplayName();
-            String email = account.getEmail();
-            String img_url = account.getPhotoUrl().toString();
-            Toast.makeText(this, email, Toast.LENGTH_LONG).show();
-            Log.d(TAG, "handleResult: INFO: " + name + " " + email + " " + img_url);
+            if (account != null) {
+                String name = account.getDisplayName();
+                String email = account.getEmail();
+                Uri photo_url = account.getPhotoUrl();
+                if (photo_url != null) {
+                    String img_url = photo_url.toString();
+                    //Loading image from below url into imageView
+                    Glide.with(this)
+                            .load(img_url)
+                            .into(imageView);
+                }
+                Toast.makeText(this, email, Toast.LENGTH_LONG).show();
+                Log.d(TAG, "handleResult: INFO: " + name + " " + email);
+
+            }
         } else {
             Log.e(TAG, "handleResult: Sync Failed");
         }
